dc9296e430e3932a78b42b5a8dee2ef146b24863
Xiaozhou Pu <Patrick_Pu@hntvlan538.1628.apn.wlan.wireless-pennnet.upenn.edu>
2017-04-10 19:15:54 -0400

    Fix priorityqueue bug in Container classes
     - Main has not been implemented
    
    [ticket: patrick_pu]

--- UserContainer.java
+++ UserContainer.java
@@ -6,22 +6,22 @@
 public class UserContainer implements Comparable<UserContainer>{
 	private User user;
 	private double similarity;
-	
+
 	public UserContainer(User user, double similarity) {
 		this.user = user;
 		this.similarity = similarity;
 	}
-	
+
 	public double getSimilarity() {
 		return similarity;
 	}
-	
+
 	public User getUser() {
 		return user;
 	}
 
 	@Override
 	public int compareTo(UserContainer o) {
-		return Double.compare(o.getSimilarity(), this.similarity);
+		return Double.compare(this.similarity, o.getSimilarity());
 	}
 }
