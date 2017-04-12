95bae54f80bf1b582d2419e6187229f71f981a68
Xiaozhou Pu <Patrick_Pu@hntvlan569.2116.wlan.wireless-resnet.upenn.edu>
2017-04-11 23:21:23 -0400

    Remove unused fields in Movie and Rating classes
     - Main is implemented to test FileReader class
     - Test successfully
    
    [ticket: patrick_pu]

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) {
 		DataCenter dc = new DataCenter();
+		System.out.print("Enter a filename: ");
 		Scanner in = new Scanner(System.in);
 		String filename = in.nextLine();
 		
